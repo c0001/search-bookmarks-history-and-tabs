28d43d6df829efaff8ff2f04ba180d21eacd029f
Entroperiance.J <bmsac0001@gmail.com>
2023-06-06 20:02:02 +0800

    [entropy/my/feature/options/builtin-daily-options] init

--- popup/js/model/options.js
+++ popup/js/model/options.js
@@ -144,6 +144,8 @@ export const defaultOptions = {
    */
   displayScore: true,
 
+  displayFavicon: true,
+
   //////////////////////////////////////////
   // TABS OPTIONS                         //
   //////////////////////////////////////////
@@ -156,12 +158,12 @@ export const defaultOptions = {
    * When initializing search, show a certain number of tabs, sorted by last visited
    * Set this to 0 to disable.
    */
-  tabsDisplayLastVisited: 0,
+  tabsDisplayLastVisited: 1000,
   /**
    * If true, the windowId of the tab result will be displayed
    * This can help to with multi-window situations
    * */
-  tabsDisplayWindowId: false,
+  tabsDisplayWindowId: true,
 
   //////////////////////////////////////////
   // HISTORY OPTIONS                      //
@@ -214,6 +216,14 @@ export const defaultOptions = {
       name: 'dict.cc',
       urlPrefix: 'https://www.dict.cc/?s=$s',
     },
+    {
+      name: 'baidu',
+      urlPrefix: 'https://www.baidu.com/s?wd=$s',
+    },
+    {
+      name: 'github',
+      urlPrefix: 'https://github.com/search?q=$s',
+    },
   ],
 
   /**
@@ -235,10 +245,28 @@ export const defaultOptions = {
       blank: 'https://www.google.com',
     },
     {
-      alias: ['d', 'dict'],
+      alias: ['dt', 'dict'],
       name: 'dict.cc',
       urlPrefix: 'https://www.dict.cc/?s=$s',
       blank: 'https://www.dict.cc',
+    },
+    {
+      alias: ['bd', 'baidu'],
+      name: 'baidu.com',
+      urlPrefix: 'https://www.baidu.com/s?wd=$s',
+      blank: 'https://www.baidu.com',
+    },
+    {
+      alias: ['gh', 'github'],
+      name: 'github.com',
+      urlPrefix: 'https://github.com/search?q=$s',
+      blank: 'https://github.com',
+    },
+    {
+      alias: ['dc', 'duck'],
+      name: 'duckduckgo.com',
+      urlPrefix: 'https://duckduckgo.com/search?q=$s',
+      blank: 'https://duckduckgo.com',
     },
   ],
 
@@ -265,19 +293,19 @@ export const defaultOptions = {
   /**
    * Base score for bookmark results
    */
-  scoreBookmarkBaseScore: 100,
+  scoreBookmarkBaseScore: 80,
   /**
    * Base score for tab results
    */
-  scoreTabBaseScore: 70,
+  scoreTabBaseScore: 100,
   /**
    * Base score for history results
    */
-  scoreHistoryBaseScore: 50,
+  scoreHistoryBaseScore: 60,
   /**
    * Base score for search engine choices
    */
-  scoreSearchEngineBaseScore: 30,
+  scoreSearchEngineBaseScore: 50,
   /**
    * Base score for custom search engine choices
    * This is set very high to ensure that it's the topmost entry
